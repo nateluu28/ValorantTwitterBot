f0991fa89bb5a1b2c289f849fd5dcc97bc466838
Nathan Luu <nateluu28@gmail.com>
2020-09-09 14:12:14 -0700

    first commit

---
+++ twitter_bot.py
@@ -0,0 +1,7 @@
+import tweepy 
+print("this is my twitter bot")
+
+CONSUMER_KEY = 'vqM0O5UcbC3eJI0wlexh82pil'
+CONSUMER_SECRET = 'OHQnLJpbQcTXKFLWKL0c2D5f4SenG1QKtWEBu8OBl9NDOsjC9w'
+ACCESS_KEY = '2282942114-95OWA1n07rzLu7KqupRecYmuyTAVICidoVhydmz'
+ACCESS_SECRET = 'xKacDrQzVidoLfaMHCunj0mMzVGxnGr8izsKzOcNtX9iN'
