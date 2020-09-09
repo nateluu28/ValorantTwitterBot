c36d0c7b6a3fdf70d3825a22f5c8c7e7299362a5
Nathan Luu <nateluu28@gmail.com>
2020-09-09 14:15:47 -0700

    added auth for tweepy

--- twitter_bot.py
+++ twitter_bot.py
@@ -5,3 +5,8 @@ CONSUMER_KEY = 'vqM0O5UcbC3eJI0wlexh82pil'
 CONSUMER_SECRET = 'OHQnLJpbQcTXKFLWKL0c2D5f4SenG1QKtWEBu8OBl9NDOsjC9w'
 ACCESS_KEY = '2282942114-95OWA1n07rzLu7KqupRecYmuyTAVICidoVhydmz'
 ACCESS_SECRET = 'xKacDrQzVidoLfaMHCunj0mMzVGxnGr8izsKzOcNtX9iN'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth .set_access_token(ACCESS_KEY,ACCESS_SECRET)
+api = tweepy.API(auth)
+
