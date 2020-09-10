7932a54d0c11bc98d52d282afff98a07d7b87851
Nathan Luu <nateluu28@gmail.com>
2020-09-10 14:26:20 -0700

    updated api keys

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,12 +1,29 @@
+import nba_api
 import tweepy 
-print("this is my twitter bot")
 
-CONSUMER_KEY = 'vqM0O5UcbC3eJI0wlexh82pil'
-CONSUMER_SECRET = 'OHQnLJpbQcTXKFLWKL0c2D5f4SenG1QKtWEBu8OBl9NDOsjC9w'
-ACCESS_KEY = '2282942114-95OWA1n07rzLu7KqupRecYmuyTAVICidoVhydmz'
-ACCESS_SECRET = 'xKacDrQzVidoLfaMHCunj0mMzVGxnGr8izsKzOcNtX9iN'
+CONSUMER_KEY = 'W3YtYRltVIpTDhFT2JkVp5Dso'
+CONSUMER_SECRET = '5Lld9LmTm66EAb9vQyYuoOpMLt8IzR8ZfFP8V6lRPKk7lpaS5y'
+ACCESS_KEY = '1304156695635976194-oqLs8Xa6DWZf9ziMXiYHsgkHq7SvQ5'
+ACCESS_SECRET = '7eBxgyxwrYbE2bWE08QJojzD0F6yQMRamjDED6ttLKmRH'
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth .set_access_token(ACCESS_KEY,ACCESS_SECRET)
 api = tweepy.API(auth)
+
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#   print(tweet.text)
+
+# user = api.get_user('nateluu28')
+# print(user.screen_name)
+# print(user.followers_count)
+
+# for friend in user.friends():
+#   print(friend.screen_name)
+
+
+mentions = api.mentions_timeline()
+
+
+print(mentions[0].text)
 
