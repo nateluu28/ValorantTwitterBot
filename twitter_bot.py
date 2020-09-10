353dad0694f3057c938a2fc571e34a84dc05fc71
Nathan Luu <nateluu28@gmail.com>
2020-09-10 15:01:00 -0700

    imported json file onto python file

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,29 +1,52 @@
-import nba_api
-import tweepy 
+import tweepy
+import json
 
 CONSUMER_KEY = 'W3YtYRltVIpTDhFT2JkVp5Dso'
 CONSUMER_SECRET = '5Lld9LmTm66EAb9vQyYuoOpMLt8IzR8ZfFP8V6lRPKk7lpaS5y'
-ACCESS_KEY = '1304156695635976194-oqLs8Xa6DWZf9ziMXiYHsgkHq7SvQ5'
-ACCESS_SECRET = '7eBxgyxwrYbE2bWE08QJojzD0F6yQMRamjDED6ttLKmRH'
+ACCESS_KEY = '1304156695635976194-8Tg6H5NwZfUjD94fXm9Kj6xAs3quWP'
+ACCESS_SECRET = 'tZF2HFscrDGoyZxQCUgNwBpkN7hXUQtAIWuNqn06rIuJN'
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth .set_access_token(ACCESS_KEY,ACCESS_SECRET)
 api = tweepy.API(auth)
 
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#   print(tweet.text)
+f = open('agents.json')
+data = json.load(f)
 
-# user = api.get_user('nateluu28')
-# print(user.screen_name)
-# print(user.followers_count)
+for i in data['agents']:
+  print(i['name'])
 
-# for friend in user.friends():
-#   print(friend.screen_name)
+
+
+agents = [
+  'jett',
+  'raze',
+  'breach',
+  'omen',
+  'brimstone',
+  'viper',
+  'phoenix',
+  'killjoy',
+  'reyna',
+  'sage',
+  'cypher'
+]
 
 
 mentions = api.mentions_timeline()
 
+for mention in mentions:
+  print(mention.text)
+  for agent in agents:
+    if agent in mention.text:
+      print(agent + 'found')
 
-print(mentions[0].text)
 
+# public_tweets = api.home_timeline()
+
+# for tweet in public_tweets:
+#   print(tweet.id)
+#   try:
+#     api.retweet(tweet.id)
+#   except tweepy.TweepError as e:
+#     print(e)
